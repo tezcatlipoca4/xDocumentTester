dd836d040bad91490a95d3e9fb27e41b81b4c19c
Tezcatlipoca <Tezcatlipoca@TEZCATLIPOCA-PC>
2018-05-26 19:57:14 +0300

    test

--- xDocumentTester/Form1.cs
+++ xDocumentTester/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace xDocumentTester
@@ -15,6 +11,102 @@ namespace xDocumentTester
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.Encoding = Encoding.UTF8;
+                string htmlCode = client.DownloadString("http://www.sport-fm.gr/tag/aris");
+
+                StringReader reader = new StringReader(htmlCode);
+                string line;
+                int articlesFound = 0;
+                bool foundStart = false;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //Μέχρι να συναντήσουμε το <div class="row"> δεν κρατάμε τπτ
+                    if (line.Contains("<div class=\"row\">"))
+                        foundStart = true;
+
+                    if (!foundStart) continue;
+
+                    if (line.Contains("<a href=\"/article/") && line.Contains("title") && !line.Contains("</a>"))
+                    {
+                        int slashFoundIndex = line.IndexOf('/');
+                        int titleFoundIndex = line.IndexOf("title");
+                        int titleEndIndex = line.IndexOf("\">");
+                        string filteredString = line.Substring(slashFoundIndex, titleFoundIndex - slashFoundIndex - 2);
+                        richTextBox1.AppendText("url= http://www.sport-fm.gr" + filteredString + "\n");
+
+                        filteredString = line.Substring(titleFoundIndex + 7, titleEndIndex - titleFoundIndex - 7);
+                        richTextBox1.AppendText("title= " + filteredString + "\n");
+                        articlesFound++;
+                    }
+                    else if (line.Contains("<span>"))
+
+                    {
+                        richTextBox1.AppendText("published at: " +
+                                                line.Replace("<span>", "").Replace("</span>", "").TrimStart() + "\n");
+                        if (articlesFound >= 15) return;
+                    }
+                    //Μόνο για το πρώτο άρθρο που έχει διαφορετική ρύθμιση για την ώρα!
+                    else if (line.Contains("article-date") && articlesFound == 1)
+                    {
+                        int stringEndTextIndex = line.IndexOf("\">");
+                        int dateEndIndex = line.IndexOf("</small>");
+                        richTextBox1.AppendText("published at: " +
+                                                line.Substring(stringEndTextIndex + 2,
+                                                    dateEndIndex - stringEndTextIndex - 2).TrimStart() + "\n");
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.Encoding = Encoding.UTF8;
+                client.Headers.Add("User-Agent: Other");
+                string htmlCode = client.DownloadString("http://www.sdna.gr/teams/paok");
+                StringReader reader = new StringReader(htmlCode);
+                int articlesFound = 0;
+                string line;
+                string htmlText = string.Empty;
+
+                //Το SDNA έχει όλες τις πληροφορίες σε μια γραμμή! Την εντοπίζουμε και την σπάμε σε σειρές ώστε να μπορέσουμε να πάρουμε τα άρθρα
+                bool foundStart = false;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //Μέχρι να συναντήσουμε το <div class="row"> που είναι και η μοναδική γραμμή που θέλουμε
+                    if (!line.Contains("<div class=\"external-wrapper\">")) continue;
+                    htmlText = (line.TrimStart().Replace("<div", "\n<div") + "\n");
+                    break;
+                }
+                //Εξάγουμε τις πληροφορίες από το string που δημιουργήσαμε
+                //reader = new StringReader(htmlText);
+                //while ((line = reader.ReadLine()) != null)
+                //{
+                //    //Στη δομή του XML πρώτα συναντάμε την ημερομηνία δημοσίευσης και μετά το άρθρο
+
+                //    if (line.Contains("<em class=\"placeholder\">"))
+                //    {
+                //        richTextBox1.AppendText("published at: " + line.Replace("<em class=\"placeholder\">", "") + "\n");
+                //    }
+
+                //}
+
+
+                richTextBox1.AppendText(htmlText);
+            }
         }
     }
 }
